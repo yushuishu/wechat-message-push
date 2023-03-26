038a884c6408e23c8a8f0cf3d512857379e6b2e9
谁书-ss <yushuishu@qq.com>
2023-03-26 11:16:48 +0800

    feat:WeChat Message Push

--- src/main/java/com/shuishu/wechat/message/push/constants/MessageConstants.java
+++ src/main/java/com/shuishu/wechat/message/push/constants/MessageConstants.java
@@ -11,12 +11,29 @@ package com.shuishu.wechat.message.push.constants;
  * <p></p>
  */
 public class MessageConstants {
+    /**
+     * 微秘书 key
+     */
     public static final String WEI_MI_SHU_KEY = "xxx";
+    /**
+     * 微秘书 secret
+     */
     public static final String WEI_MI_SHU_SECRET = "xxx";
+    /**
+     * 微秘书 api
+     */
     public static final String WEI_MI_SHU_API = "https://api-bot.aibotk.com/";
-
+    /**
+     * 天行数据 key
+     */
     public static final String TIAN_XING_KEY = "xxx";
+    /**
+     * 天行数据 api
+     */
     public static final String TIAN_XING_API = "https://apis.tianapi.com/";
+
+
+
 
     /**
      * 早安心语
@@ -50,4 +67,5 @@ public class MessageConstants {
      * 云音乐热评
      */
     public static final String HOT_REVIEW = "hotreview/index";
+
 }
